75d51b2faaa57de814a14056d11cd01ad850d9f4
Pentiminax <tanguy.lemarie.youtube@outlook.fr>
2024-11-27 16:08:09 +0100

    Add env loading

--- website/db.php
+++ website/db.php
@@ -2,16 +2,24 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$host = 'mysql';
+use Dotenv\Dotenv;
+
+if (file_exists(__DIR__ . '/.env.local')) {
+    $dotenv = Dotenv::createImmutable(__DIR__, ['.env.local']);
+} else {
+    $dotenv = Dotenv::createImmutable(__DIR__);
+}
+
+$dotenv->load();
+
+$host = $_ENV['MYSQL_HOST'];
 $db = $_ENV['MYSQL_DATABASE'];
-$user = $_ENV['MYSQL_USER'];
+$username = $_ENV['MYSQL_USER'];
 $password = $_ENV['MYSQL_PASSWORD'];
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Erreur de connexion : " . $e->getMessage());
+    echo "Connection failed: " . $e->getMessage();
 }
-
-?>
